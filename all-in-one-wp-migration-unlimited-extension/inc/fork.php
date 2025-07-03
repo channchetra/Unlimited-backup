a749efc6c8b7ef723cb896ee8a9822e6dd8fe161
Stingray82 <11146912+stingray82@users.noreply.github.com>
2025-07-04 00:20:39 +0100

    Version 2.72.3 Release

--- all-in-one-wp-migration-unlimited-extension/inc/fork.php
+++ all-in-one-wp-migration-unlimited-extension/inc/fork.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // === Remove Ai1wm "Check for Updates" and "Contact Support" Links ===
 add_filter( 'plugin_row_meta', 'rup_unlimited_backup_ai1wmue_filter_plugin_row_meta', 15, 2 );
 function rup_unlimited_backup_ai1wmue_filter_plugin_row_meta( $plugin_meta, $plugin_file ) {
-	if ( $plugin_file === 'unlimited-backup-ai1wmue/unlimited-backup-ai1wmue.php' ) {
+	if ( $plugin_file === 'all-in-one-wp-migration-unlimited-extension/all-in-one-wp-migration-unlimited-extension.php' ) {
 		$plugin_meta = array_filter( $plugin_meta, function( $meta ) {
 			return strpos( $meta, 'ai1wm_check_for_updates' ) === false
 				&& strpos( $meta, 'servmask.com/help' ) === false // Typical support link
@@ -43,8 +43,8 @@ add_action( 'plugins_loaded', 'rup_unlimited_backup_ai1wmue_filter_ai1wm_extensi
 function rup_unlimited_backup_ai1wmue_filter_ai1wm_extensions() {
 	if ( class_exists( 'Ai1wm_Extensions' ) && method_exists( 'Ai1wm_Extensions', 'get' ) ) {
 		$original_get = \Ai1wm_Extensions::get();
-		if ( isset( $original_get['ai1wm-unlimited-fork'] ) ) {
-			unset( $original_get['ai1wm-unlimited-fork'] );
+		if ( isset( $original_get['ai1wm-unlimited'] ) ) {
+			unset( $original_get['ai1wm-unlimited'] );
 		}
 		$GLOBALS['rup_unlimited_backup_ai1wmue_filtered_extensions'] = $original_get;
 
